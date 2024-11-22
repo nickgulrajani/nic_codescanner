514703ebfbeeb8763739012df3c7b0baae0ad946
Nick Gulrajani <nicholas.gulrajani@gmail.com>
2024-11-22 18:01:06 -0500

    WIP

--- src/factorial.py
+++ src/factorial.py
@@ -49,7 +49,7 @@ def factorial_recursive(n):
 
 # Example usage
 if __name__ == "__main__":
-    print("Hello Bank Team Welcome to Factorial Calculator!")
+    print("Hello 'TD' Bank Team Welcome to Factorial Calculator!")
     print("--------------------------------")
     try:
         num = int(input("Enter a non-negative integer: "))
